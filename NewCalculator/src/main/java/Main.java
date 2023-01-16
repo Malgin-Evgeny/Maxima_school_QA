f480e4e5f2abf6e98be98c5ea925d99a8e13de51
Мальгин Евгений Владимирович <115340327+Malgin-Evgeny@users.noreply.github.com>
2023-01-16 21:06:09 +0300

    Add files via upload

---
+++ NewCalculator/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import Calculator.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+            Calculator calculator = new Calculator();
+            calculator.getResult();
+            System.out.println(calculator.getResult());
+        }
+    }
