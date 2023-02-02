ff311f0d08287f019f052b3fb9a420ebffb285dc
Мальгин Евгений Владимирович <115340327+Malgin-Evgeny@users.noreply.github.com>
2023-02-02 20:24:58 +0300

    Add files via upload

--- Maven_Project/src/main/java/org/example/Main.java
+++ Maven_Project/src/main/java/org/example/Main.java
@@ -1,33 +1,21 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-        driver.get("https://www.gosuslugi.ru/");
+        driver.manage().window().setSize(new Dimension(1980, 1020));
+        driver.get("https://the-internet.herokuapp.com");
 
-        WebElement linkToCheckboxesOne = driver.findElement(By.cssSelector(".button.font-[tabindex=\"11\"]"));
-        linkToCheckboxesOne.click();
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+        allLinks.get(10).click();
 
-        WebElement linkToCheckboxesTwo = driver.findElement(By.cssSelector("#login.plain-input"));
-        linkToCheckboxesTwo.sendKeys("79527778855");
-
-        WebElement linkToCheckboxesThree = driver.findElement(By.cssSelector("#password.plain-input"));
-        linkToCheckboxesThree.sendKeys("Test1234");
-
-        WebElement linkToCheckboxes = driver.findElement(By.cssSelector(".plain-button.plain-button_wide"));
-        linkToCheckboxes.click();
         driver.quit();
-
-
-
-
     }
 }
