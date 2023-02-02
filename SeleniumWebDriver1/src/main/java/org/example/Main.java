661afe62d8fe0e36991e55ed7a4a10464db624fa
Мальгин Евгений Владимирович <115340327+Malgin-Evgeny@users.noreply.github.com>
2023-02-02 20:32:19 +0300

    Add files via upload

---
+++ SeleniumWebDriver1/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1980, 1020));
+        driver.get("https://the-internet.herokuapp.com");
+
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+        allLinks.get(10).click();
+
+        driver.quit();
+    }
+}
