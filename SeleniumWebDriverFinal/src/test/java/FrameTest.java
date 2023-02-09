aa662c0759e665cbbcd084055a3bd309601eda31
Мальгин Евгений Владимирович <115340327+Malgin-Evgeny@users.noreply.github.com>
2023-02-09 20:49:15 +0300

    Add files via upload

---
+++ SeleniumWebDriverFinal/src/test/java/FrameTest.java
@@ -0,0 +1,79 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class FrameTest {
+    public WebDriver driver;
+
+    @Test
+    public void workWithFrame() {
+
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1980, 1020));
+        driver.get("https://the-internet.herokuapp.com/nested_frames");
+
+        driver.switchTo().frame("frame-top");
+        driver.switchTo().frame("frame-left");
+
+        String leftFrameText = driver.findElement(By.tagName("body")).getText();
+        Assert.assertEquals(leftFrameText, "LEFT");
+    }
+
+    @Test
+    public void workWithWindows() {
+
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1980, 1020));
+        driver.get("https://the-internet.herokuapp.com/windows");
+        String initialHandle = driver.getWindowHandle();
+
+        driver.findElement(By.cssSelector("[href='/windows/new']")).click();
+
+        String newTabHandle = driver.getWindowHandles().toArray()[1].toString();
+
+        driver.switchTo().window(newTabHandle);
+
+        String text = driver.findElement(By.tagName("h3")).getText();
+
+        Assert.assertEquals(text, "New Window");
+
+        driver.close();
+        driver.switchTo().window(initialHandle);
+    }
+
+    @Test
+    public void workWithShadowDom() throws InterruptedException {
+
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1980, 1020));
+
+        driver.get("https://canvas.apps.chrome/");
+
+        WebElement drawingApp = driver.findElement(By.id("drawing-app"));
+        SearchContext drawingAppSR = getShadowRoot(drawingApp);
+
+        WebElement welcomeDialog = drawingAppSR.findElement(By.cssSelector("welcome-dialog"));
+        SearchContext welcomeDialogSR = getShadowRoot(welcomeDialog);
+
+        WebElement getStartedButton = welcomeDialogSR.findElement(By.cssSelector("#get-started"));
+        Thread.sleep(3000);
+        getStartedButton.click();
+        Thread.sleep(3000);
+
+        driver.switchTo().window(driver.getWindowHandles().toArray()[1].toString());
+        driver.close();
+    }
+
+    private SearchContext getShadowRoot(WebElement rootElement) {
+        return (SearchContext)((JavascriptExecutor) driver)
+                .executeScript("return arguments[0].shadowRoot", rootElement);
+    }
+
+    @AfterMethod
+    public void closeDrive(){
+        driver.quit();
+    }
+}
+
