ad43c33ba8fe666d20c648df9e265c33df718473
Мальгин Евгений Владимирович <115340327+Malgin-Evgeny@users.noreply.github.com>
2023-02-13 20:56:48 +0300

    Update Test1.java

--- SeleniumWebDriver1/src/test/java/Test1.java
+++ SeleniumWebDriver1/src/test/java/Test1.java
@@ -8,10 +8,9 @@ public class Test1 {
     @Test
     public void joinDragAndDropPage() {
         WebDriver driver = new ChromeDriver();
+        MainPage mainPage = new MainPage(driver);
         driver.manage().window().setSize(new Dimension(1980, 1020));
         driver.get("https://the-internet.herokuapp.com");
-
-        MainPage mainPage = new MainPage(driver);
 
         mainPage.setList(10);
 
