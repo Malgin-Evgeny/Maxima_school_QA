3ca5289db423f9b36003d8349667d1cec2c8a16c
Мальгин Евгений Владимирович <115340327+Malgin-Evgeny@users.noreply.github.com>
2023-02-24 16:06:53 +0300

    Add files via upload

---
+++ Singleton/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Logger.getLogger().addLog("Первый лог ");
+        Logger.getLogger().addLog("Второй лог ");
+        Logger.getLogger().addLog("Третий лог ");
+        Logger.getLogger().addLog("Четвертый лог ");
+        Logger.getLogger().addLog("Пятый лог ");
+
+        Logger.getLogger().showLogFile();
+    }
+}
