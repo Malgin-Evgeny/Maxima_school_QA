4bb107685cc7e768d7c422ae8e1f7952ab614c3b
Мальгин Евгений Владимирович <115340327+Malgin-Evgeny@users.noreply.github.com>
2023-05-04 20:17:04 +0300

    Delete src directory

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import Calculator.Calculator;
-
-public class Main {
-    public static void main(String[] args) {
-            Calculator calculator = new Calculator();
-            calculator.getResult();
-            System.out.println(calculator.getResult());
-        }
-    }
