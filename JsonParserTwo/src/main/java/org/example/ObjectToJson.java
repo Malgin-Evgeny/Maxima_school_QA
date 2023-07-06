d4befca6434e243f4074802a614caa88e96dbb92
Eugenyy Malgin <eugenimalgin@yandex.ru>
2023-07-06 20:20:22 +0300

    Add new Parser

--- JsonParserTwo/src/main/java/org/example/ObjectToJson.java
+++ JsonParserTwo/src/main/java/org/example/ObjectToJson.java
@@ -6,25 +6,25 @@ import java.util.Map;
 import java.util.StringJoiner;
 
 public class ObjectToJson {
-    public String convertToJson (Object o){
+    public String convertToJson(Object o) {
         try {
             return getJsonToString(o);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Converting has benn broken");
         }
     }
 
-    private String getJsonToString(Object o) throws Exception{
-       Class<?> clazz = o.getClass();
-        Map<String,String> jsonElements = new HashMap<>();
-        for (Field field : clazz.getDeclaredFields()){
+    private String getJsonToString(Object o) throws Exception {
+        Class<?> clazz = o.getClass();
+        Map<String, String> jsonElements = new HashMap<>();
+        for (Field field : clazz.getDeclaredFields()) {
             field.setAccessible(true);
-            if(field.isAnnotationPresent(JsonElement.class)){
+            if (field.isAnnotationPresent(JsonElement.class)) {
                 jsonElements.put(getKey(field), (String) field.get(o));
             }
         }
         StringJoiner joiner = new StringJoiner(",");
-        for (Map.Entry<String, String> entry : jsonElements.entrySet()){
+        for (Map.Entry<String, String> entry : jsonElements.entrySet()) {
             String s = "\"" + entry.getKey() + "\":\""
                     + entry.getValue() + "\"";
             joiner.add(s);
@@ -33,10 +33,10 @@ public class ObjectToJson {
         return "{" + jsonString + "}";
     }
 
-    private String getKey(Field field){
+    private String getKey(Field field) {
         JsonElement jsonElement = field.getAnnotation(JsonElement.class);
         String key = jsonElement.key();
-        if (key.equals("")){
+        if (key.equals("")) {
             return field.getName();
         } else {
             return key;
